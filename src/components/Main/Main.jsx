641bab28f1ecfb2900ad62ac0490e06221dbe511
Geovane Dias de Araujo <Diasgeovane7@gmail.com>
2024-10-20 20:27:21 -0300

    Terminado a estilização inicial do modelo para PC.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,12 +7,14 @@ export default function Main() {
 
   return (
     <main>
-      <p>"Liberte a magia da programação com a lâmpada de Aladim."</p>
-      <img 
-        src={lampada ? "genio.png" : "lampadaMagica.png"}
-        alt="Lâmpada Mágica" 
-      />
-      <button onClick={trocarLampada}>Clique Aqui</button>
+      <section>
+        <p>"Liberte a magia da programação com a lâmpada de Aladim."</p>
+        <img
+          src={lampada ? "genio.png" : "lampadaMagica.png"}
+          alt="Lâmpada Mágica"
+        />
+        <button onClick={trocarLampada}>Clique Aqui</button>
+      </section>
     </main>
   );
 };
